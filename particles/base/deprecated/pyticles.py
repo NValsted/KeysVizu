2b13211d05666da3a71392adb855ce08d1b945d5
NValsted <nicolaj.valsted@live.dk>
2021-01-26 21:29:45 +0100

    Finished particles proof-of-concept in python

--- particles/base/deprecated/pyticles.py
+++ particles/base/deprecated/pyticles.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 from collections import deque
 
-#import fluids
+from . import fluids
 
 class Particle:
     coords = [] # x,y
@@ -17,9 +17,10 @@ class Particle:
 
 class ParticleSystem:
     particles = deque()
+    fluid = None
 
-    def __init__(self):
-        pass
+    def __init__(self,fluid):
+        self.fluid = fluid
 
     def spawn_particles(self,N = 1,
                         angle = 0, speed = 1, size = 1,
@@ -47,11 +48,28 @@ class ParticleSystem:
         for particle in self.particles:
             if particle.age > 42:
                 to_be_popped_count += 1
-
+            
+            particle.velocity[0] += self.fluid.velocity_x[int(particle.coords[0])+32][int(particle.coords[1])+32]
+            particle.velocity[1] += self.fluid.velocity_y[int(particle.coords[0])+32][int(particle.coords[1])+32]
+            
             particle.velocity[0] -= (particle.velocity[0] * 0.05) # drag
-            particle.velocity[1] -= (particle.velocity[1] * 0.05) # drag
+            particle.velocity[1] -= (particle.velocity[1] * 0.05) # drag 
+
             particle.coords[0] += particle.velocity[0]
             particle.coords[1] += particle.velocity[1]
+            
+            if particle.coords[0] > 31:
+                particle.coords[0] = 31
+
+            if particle.coords[1] > 31:
+                particle.coords[1] = 31
+
+            if particle.coords[0] < -32:
+                particle.coords[0] = -32
+
+            if particle.coords[1] < -32:
+                particle.coords[1] = -32
+
             particle.age += 1
 
         while to_be_popped_count:
@@ -60,13 +78,11 @@ class ParticleSystem:
 
 
 def main():
-    FluidField = fluids.Fluid(32,0.01,0.01,0.01)
-    FluidField.iterate()
+    FluidField = fluids.Fluid(64,0.01,0.01,0.01)
 
-    particle_system = ParticleSystem()
+    particle_system = ParticleSystem(FluidField)
     particle_system.spawn_particles(N=3)
     for i in range(100):
-        
         
         print("At iteration:", i)
         for p in particle_system.particles:
--- particles/particles_demo.py
+++ particles/particles_demo.py
@@ -9,6 +9,7 @@ from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
 
 from base.deprecated import pyticles
+from base.deprecated import fluids
 
 import random
 
@@ -18,18 +19,35 @@ class ParticleScene(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        self.particle_system = pyticles.ParticleSystem()
+        self.fluid_field = fluids.Fluid(64,0.01,0.01,0.01)
+        self.particle_system = pyticles.ParticleSystem(self.fluid_field)
         Clock.schedule_interval(lambda dt: self.update_system(),1/30)
         
     def update_system(self):
-        self.particle_system.spawn_particles(N=10,speed=0.5,angle_spread=3.14,magnitude_spread=1)
+        self.particle_system.spawn_particles(N=random.randint(-100,30),speed=0.5,angle_spread=3.14,magnitude_spread=1)
+        #self.fluid_field.add_velocity(random.randint(27,37),random.randint(27,37),(random.random()-0.5)*5,(random.random()-0.5)*5)
+        
         self.particle_system.iterate()
+        self.fluid_field.iterate()
 
         self.canvas.clear()
         with self.canvas:
             for particle in self.particle_system.particles:
                 Color(random.random(),(42-particle.age)/42,1,(42-particle.age)/42)
                 Rectangle(pos=((particle.coords[0]+N//2)*10,(particle.coords[1]+N//2)*10),size=(3,3))
+
+    def on_touch_down(self,touch):
+        self.touch_down_pos = touch.pos
+
+    def on_touch_up(self,touch):
+        x_diff = touch.pos[0] - self.touch_down_pos[0]
+        y_diff = touch.pos[1] - self.touch_down_pos[1]
+
+        x_pos = int(self.touch_down_pos[0]/10)
+        y_pos = int(self.touch_down_pos[1]/10)
+
+        drag_multiplier = 0.5
+        self.fluid_field.add_velocity(x_pos,y_pos,x_diff*drag_multiplier,y_diff*drag_multiplier)
 
 class Scene(Widget):
     pass
